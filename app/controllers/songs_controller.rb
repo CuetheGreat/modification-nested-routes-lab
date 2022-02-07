7db5c07801d4f436e575f0969223b5787644912f
Clayon Stewart <c.stewart89@gmail.com>
2022-02-07 13:56:24 -0500

    commit tree 3c0e53644a929471f79ac999e17f7fba77be7d92

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,11 +26,17 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    if params[:artist_id]
+      if artist = Artist.find_by(id: params[:artist_id])
+        @song.artist = artist
+      else
+        redirect_to artists_path(artist), alert: "artist cannot be found"
+      end
+    end
   end
 
   def create
     @song = Song.new(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -39,7 +45,19 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist Could not be found"
+      else
+        @song = @artist.songs.find_by(id: params[:id])
+        if @song.nil?
+          redirect_to artist_songs_path, alert: "Artist Could not be found"
+        end
+      end
+    else
+      @song = Song.find_by(id: params[:id])
+    end
   end
 
   def update
@@ -64,7 +82,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
